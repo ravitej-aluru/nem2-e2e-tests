6fc8758b02d267687a69a2f58fcd9594b87e9a56
Aluru, Ravitej <14794711+ravitej-aluru@users.noreply.github.com>
2020-02-05 21:10:27 +0000

    improved name parser

--- chaos-tests/name_parser.py
+++ chaos-tests/name_parser.py
@@ -3,6 +3,7 @@
 import yaml
 import os
 import sys
+import logging
 
 
 def get_docker_container_names(compose_file):
@@ -15,9 +16,22 @@ def get_docker_container_names(compose_file):
     print(docker_containers)
     return docker_containers
 
+
+def get_relative_file_path(file_name):
+    logging.debug("Looking for file {}", file_name)
+    relative_file_path = [os.path.join(dirpath, filename) for dirpath, _, filenames in os.walk('.') for filename in filenames if filename.endswith(file_name)]
+    if len(relative_file_path) > 1:
+        logging.warning("More than one docker-compose file with the same name found in sub-directories %s. Returning the first one.", relative_file_path)
+        return relative_file_path[0]
+    elif len(relative_file_path) == 0:
+        logging.error("docker-compose file '%s' not found!", file_name)
+    else:
+        return relative_file_path[0]
+
+
 if __name__ == "__main__":
-    kill_compose_file = sys.argv[1]
-    print(kill_compose_file)
-    kill_file_path = [os.path.join(dirpath, filename) for dirpath, _, filenames in os.walk('.') for filename in filenames if filename.endswith(kill_compose_file)]
-    print(kill_file_path)
-    get_docker_container_names(kill_file_path[0])
+    compose_file = sys.argv[1]
+    logging.debug("docker-compose file name: {}", compose_file)
+    file_path = get_relative_file_path(compose_file)
+    logging.debug("Path of the compose file: {}", file_path)
+    get_docker_container_names(file_path)
