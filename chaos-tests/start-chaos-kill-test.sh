bf8f184313b02a4c24a9a2df54b4087152edce7d
Aluru, Ravitej <14794711+ravitej-aluru@users.noreply.github.com>
2020-02-05 21:12:53 +0000

    working parameterised chaos kill script

--- chaos-tests/start-chaos-kill-test.sh
+++ chaos-tests/start-chaos-kill-test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# set -e to exit if the time provided by argument 1 is not valid for date.
+# set -e to exit if the time provided by argument 2 is not valid for date.
 # The variable STOP_TIME will store the seconds since 1970-01-01 00:00:00
 # UTC, according to the date specified by -d "$2".
 
@@ -28,32 +28,30 @@ echo -e "Starting test at $(date)"
 echo -e "Expected finish time: $(date -d "$STOP_TIME")"
 echo -e "Using chaos docker-compose file: $KILL_COMPOSE_FILE"
 
+# set -x
 # CHAOS_LOG_FILE=chaos-logs/$KILL_COMPOSE_FILE.$(date +"%d.%m.%Y-%H.%M.%S").log
 # Launch the catapult server and pumba containers
 docker-compose -f ../catapult-bootstrap/cmds/docker/docker-compose.yml -f ${KILL_COMPOSE_FILE} up -d
+# Call the python script and then remove the square braces [] from the output
+DOCKER_CONTAINERS=($(python3 name_parser.py $KILL_COMPOSE_FILE | tr -d '[],'))
+echo "List of containers: ${DOCKER_CONTAINERS[@]}"
+sleep 10
 docker ps
-docker ps --filter NAME=docker_peer-node-0_1 --filter NAME=docker_peer-node-1_1
 
 # Repeat the loop while the current date is less than STOP_TIME_EPOCH_SECONDS
 while [ $(date "+%s") -lt ${STOP_TIME_EPOCH_SECONDS} ]; do
   sleep 60
-  docker ps --filter NAME=docker_peer-node-0_1 --filter NAME=docker_peer-node-1_1
-
-  NODE_0_STATUS=$(docker inspect docker_peer-node-0_1 --format='{{.State.Status}}')
-  NODE_0_EXITCODE=$(docker inspect docker_peer-node-1_1 --format='{{.State.ExitCode}}')
-
-  if [ ${NODE_0_STATUS} == "exited"  ] && [ ${NODE_0_EXITCODE} == "137" ]; then
-    echo "peer-node-0 was randomly killed by pumba, restarting it."
-    docker start docker_peer-node-0_1
-  fi
-
-  NODE_1_STATUS=$(docker inspect docker_peer-node-1_1 --format='{{.State.Status}}')
-  NODE_1_EXITCODE=$(docker inspect docker_peer-node-1_1 --format='{{.State.ExitCode}}')
-
-  if [ ${NODE_1_STATUS} == "exited" ] && [ ${NODE_1_EXITCODE} == "137" ]; then
-    echo "peer-node-1 was randomly killed by pumba, restarting it."
-    docker start docker_peer-node-1_1
-  fi
+  for container in "${DOCKER_CONTAINERS[@]}"; do
+    # Remove the single quotes from the container name string (it was returned by python with '')
+    container=$(sed s/\'//g <<<$container)
+    echo "Processing docker container: $container"
+    NODE_STATUS=$(docker inspect $container --format='{{.State.Status}}')
+    NODE_EXITCODE=$(docker inspect $container --format='{{.State.ExitCode}}')
+    echo "container $container status: ${NODE_STATUS}; exitcode: ${NODE_EXITCODE}"
+    if [ ${NODE_STATUS} == "exited"  ] && [ ${NODE_EXITCODE} == "137" ]; then
+      echo "$container was randomly killed by pumba, restarting it."
+      docker start $container
+    fi
+  done
 done
-
 docker-compose -f ../catapult-bootstrap/cmds/docker/docker-compose.yml -f ${KILL_COMPOSE_FILE} down --remove-orphans
