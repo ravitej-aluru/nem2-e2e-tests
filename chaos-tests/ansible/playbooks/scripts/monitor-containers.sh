9433f87bb5f85a1fb23b40c570a294af7b9c4f19
Aluru, Ravitej <14794711+ravitej-aluru@users.noreply.github.com>
2020-06-06 07:14:27 +0100

    Dockerised monitor containers script

--- chaos-tests/ansible/playbooks/scripts/monitor-containers.sh
+++ chaos-tests/ansible/playbooks/scripts/monitor-containers.sh
@@ -4,53 +4,69 @@
 # The variable STOP_TIME will store the seconds since 1970-01-01 00:00:00
 # UTC, according to the date specified by -d "$2".
 
-# $ ./xxxxxx.sh ... "1 hour 4 minutes 3 seconds" ...
-# $ ./xxxxxx.sh ... "tomorrow 8:00am" ...
-# $ ./xxxxxx.sh ... "monday 8:00am" ...
+# Set some default values:
+API_NODE_CONTAINER_NAME=
+API_BROKER_CONTAINER_NAME=
+STOP_TIME=unset
+SLEEP_INTERVAL=unset
+DOCKER_CONTAINERS=unset
+START_TIME_EPOCH_SECONDS=unset
+STOP_TIME_EPOCH_SECONDS=unset
 
-PARAMS=""
-while (( "$#" )); do
+usage()
+{
+  echo "Usage: 
+    monitor-containers [ -a | --api-node API_NODE_CONTAINER_NAME ]
+                       [ -b | --api-broker API_BROKER_CONTAINER_NAME ]
+                       -e | --end-time 20:00 | 2020.12.30-08:00
+                          # Recognized TIME formats:
+                          #   hh:mm[:ss]
+                          #   [YYYY.]MM.DD-hh:mm[:ss]
+                          #   YYYY-MM-DD hh:mm[:ss]
+                          #   [[[[[YY]YY]MM]DD]hh]mm[.ss]
+                       -s | --sleep-interval (seconds) 60
+                       container name(s)"
+  exit 2
+}
+
+parse_and_validate_end_time()
+{
+    set -e
+    STOP_TIME="$@"
+    STOP_TIME=$(sed s/\'//g <<<$STOP_TIME)
+    S=$(date -d "$STOP_TIME" "+%s")
+    set +e 
+}
+
+PARSED_ARGUMENTS=$(getopt -a -n monitor-containers -o a:b:e:s: --long api-node:,api-broker:,end-time:,sleep-interval: -- "$@")
+VALID_ARGUMENTS=$?
+if [ "$VALID_ARGUMENTS" != "0" ]; then
+  usage
+fi
+
+echo "$(date)::PARSED_ARGUMENTS: $PARSED_ARGUMENTS"
+eval set -- "$PARSED_ARGUMENTS"
+while :
+do
   case "$1" in
-    -a|--api-node)
-      if [ -n "$2" ] && [ ${2:0:1} != "-" ]; then
-        API_NODE_CONTAINER_NAME=$2
-        shift 2
-      else
-        echo "Error: Argument for $1 is missing" >&2
-        exit 1
-      fi
-      ;;
-    -b|--api-broker)
-      if [ -n "$2" ] && [ ${2:0:1} != "-" ]; then
-        API_BROKER_CONTAINER_NAME=$2
-        shift 2
-      else
-        echo "Error: Argument for $1 is missing" >&2
-        exit 1
-      fi
-      ;;
-    -*|--*=) # unsupported flags
-      echo "Error: Unsupported flag $1" >&2
-      exit 1
-      ;;
-    *) # preserve positional arguments
-      PARAMS="$PARAMS $1"
-      shift
-      ;;
+    -a | --api-node)        API_NODE_CONTAINER_NAME="$2"    ; shift 2 ;;
+    -b | --api-broker)      API_BROKER_CONTAINER_NAME="$2"  ; shift 2 ;;
+    -e | --end-time)        parse_and_validate_end_time $2  ; shift 2 ;;
+    -s | --sleep-interval)  SLEEP_INTERVAL="$2"             ; shift 2 ;;
+    # -- means the end of the arguments; drop this, and break out of the while loop
+    --) shift; break ;;
+    # If invalid options were passed, then getopt should have reported an error,
+    # which we checked as VALID_ARGUMENTS when getopt was called...
+    *) echo "Unexpected option: $1 - this should not happen."
+       usage ;;
   esac
 done
-# set positional arguments in their proper place
-eval set -- "$PARAMS"
 
-# set -e to exit if the time provided by argument 2 is not valid for date.
-set -e
-STOP_TIME=$1
-set +e
-SLEEP_INTERVAL=$2
-DOCKER_CONTAINERS=(${@:3})
+DOCKER_CONTAINERS=($@)
+
 if [[ ! -z "${API_BROKER_CONTAINER_NAME}" ]]
 then
-  echo "$(date)::API broker container to be monitored: ${API_BROKER_CONTAINER_NAME}"
+  echo "$(date)::API broker container to monitor   : ${API_BROKER_CONTAINER_NAME}"
   DOCKER_CONTAINERS+=("${API_BROKER_CONTAINER_NAME}")
 else
   echo "$(date)::API broker container not specified"
@@ -58,31 +74,33 @@ fi
 
 if [[ ! -z "${API_NODE_CONTAINER_NAME}" ]]
 then
-  echo "$(date)::API node container to be monitored: ${API_NODE_CONTAINER_NAME}"
+  echo "$(date)::API node container to monitor     : ${API_NODE_CONTAINER_NAME}"
   DOCKER_CONTAINERS+=("${API_NODE_CONTAINER_NAME}")
 else
   echo "$(date)::API node container not specified"
 fi
 
-echo "$(date)::List of Symbol Docker containers to be monitored: ${DOCKER_CONTAINERS[@]}"
+echo "$(date)::List of Symbol containers to monitor: ${DOCKER_CONTAINERS[@]}"
 
 START_TIME_EPOCH_SECONDS=$(date "+%s")
 STOP_TIME_EPOCH_SECONDS=$(date -d "$STOP_TIME" "+%s")
 
-echo -e "\nStart time: $(date)"
-echo -e "End time: $(date -d "$STOP_TIME")\n"
+echo -e "\n$(date)::Start time                     : $(date)"
+echo -e "$(date)::End time                       : $(date -d "$STOP_TIME")\n"
 
 # Repeat the loop while the current date is less than STOP_TIME_EPOCH_SECONDS
 while [ $(date "+%s") -lt ${STOP_TIME_EPOCH_SECONDS} ]; do
   sleep $SLEEP_INTERVAL
   for container in "${DOCKER_CONTAINERS[@]}"; do
     # Remove the single quotes from the container name string (it was returned by python with '')
-    echo "$(date)::Processing docker container: $container"
+    echo "$(date)::Processing docker container         : $container"
     container=$(sed s/\'//g <<<$container)
-    echo "$(date)::Processing docker container: $container"
+    echo "$(date)::Processing docker container         : $container"
     NODE_STATUS=$(docker inspect $container --format='{{.State.Status}}')
     NODE_EXITCODE=$(docker inspect $container --format='{{.State.ExitCode}}')
-    echo "$(date)::container $container status: ${NODE_STATUS}; exitcode: ${NODE_EXITCODE}"
+    echo "$(date):: container $container 
+                    status     : ${NODE_STATUS}
+                    exitcode   : ${NODE_EXITCODE}"
     if [[ ${NODE_STATUS} == "exited"  ]] && [[ ${NODE_EXITCODE} == "137" ]]; then
       
       # If it is the api-node-broker, then stop the api-node, start the broker, then start the api-node to avoid failure conditions
